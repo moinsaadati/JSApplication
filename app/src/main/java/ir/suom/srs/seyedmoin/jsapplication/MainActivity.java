ee37036e82b75977777f0cb42760b44822d5eb2f
Seyed Moin Saadati <saadati.moin@gmail.com>
2017-02-16 22:30:37 +0330

    First Test (Show a Massage With Toast ) : OK

--- app/src/main/java/ir/suom/srs/seyedmoin/jsapplication/MainActivity.java
+++ app/src/main/java/ir/suom/srs/seyedmoin/jsapplication/MainActivity.java
@@ -1,16 +1,62 @@
 package ir.suom.srs.seyedmoin.jsapplication;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    WebView webView;
+
+    @SuppressLint({"SetJavaScriptEnabled", "JavascriptInterface", "AddJavascriptInterface"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
 
+        webView.setWebChromeClient(new WebChromeClient() {
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                return false;
+            }
+        });
+
+        webView.loadUrl("file:///android_asset/ihtml.html");
+
+        webView.addJavascriptInterface(new WebAppInterface(this), "app_moin");
+
+
+    }
+
+    //Class to be injected in Web page
+    public class WebAppInterface {
+        Context mContext;
+
+        /**
+         * Instantiate the interface and set the context
+         */
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        /**
+         * Show Toast Message
+         *
+         * @param toast
+         */
+        @JavascriptInterface
+        public void show(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
